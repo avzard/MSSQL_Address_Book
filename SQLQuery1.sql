cb17d328b18a7340cf9fec44f81bd393ea1daa87
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-22 20:00:32 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -16,8 +16,8 @@ select * from AddressBook;
 
 --UC3 insert contact details into table--
 insert into AddressBook(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
-values('Shivaraj','Gowda','DGG','DGG','CG',491445,9689556677,'raj@gmail.com'),
-('Amar','Jeet','Bhilai','Bhilai','CG',490020,9644556677,'amar@gmail.com'),
-('Aviral','Kumar','Raipur','Raipur','CG',492001,9644556677,'avi@gmail.com'),
-('Nikhil','Yadav','Bhilai','Bhilai','CG',490020,9644556677,'nikhil@gmail.com');
+values('Abhishek','Jadhav','DGG','DGG','CG',491445,9689556677,'raj@gmail.com'),
+('Raju','Bandi','Amb','Amb','Amb',490020,9644556677,'dd@gmail.com'),
+('Nadeem','Khan','Bl','bl','CG',492001,9644556677,'ND@gmail.com'),
+('Harshad','Nachan','Bhilai','Bhilai','CG',490020,9644556677,'nachan@gmail.com');
 select * from AddressBook;
